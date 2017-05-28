f68ea6ef6d34281299658f052eeb084898f8183f
Alex Pelan <alexpelan@gmail.com>
2017-05-28 18:59:12 -0400

    Some changes for using AWS as a backend instead of localhost
    -New constant for public domain of API server
    -Temporarily set NSExceptionAllowsInsecureHTTPLoads so we can test
    with the http endpoint I currently have set up at AWS - eventually
    we'll have HTTPS (before launch) and this can be set back to a
    whitelist with localhost.

--- app/util/Consts.js
+++ app/util/Consts.js
@@ -1,11 +1,13 @@
+const DOMAIN = "http://ec2-54-68-99-81.us-west-2.compute.amazonaws.com:3000";
+
 const CONSTS = {
 	JEOPARDY: "jeopardy",
 	DOUBLE_JEOPARDY: "double_jeopardy",
 	FINAL_JEOPARDY: "final_jeopardy",
-	GAME_REQUEST_URL: 'http://localhost:3000/api/games/',
-	SEASONS_REQUEST_URL: "http://localhost:3000/api/",
-	GAME_LIST_REQUEST_URL: "http://localhost:3000/api/seasons/",
-	DISPUTE_URL: "http://localhost:3000/api/dispute",
+	GAME_REQUEST_URL: DOMAIN + '/api/games/',
+	SEASONS_REQUEST_URL: DOMAIN + "/api/",
+	GAME_LIST_REQUEST_URL: DOMAIN + "/api/seasons/",
+	DISPUTE_URL: DOMAIN + "/api/dispute",
 	MILLISECONDS_IN_DAY: 24 * 60 * 60 * 1000,
 };
 
