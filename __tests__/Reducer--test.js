78c6c88b450167481458a12f6949619e9c268972
Alex Pelan <alexpelan@gmail.com>
2017-09-09 11:20:07 -0400

    Wrap up reducer tests
    defaultState was getting mutated between tests before, so use
    Object.assign() instead to use a copy that doesn't cause changes by
    reference.

--- __tests__/Reducer--test.js
+++ __tests__/Reducer--test.js
@@ -9,13 +9,15 @@ const seasonList = Factory.seasonList();
 const timeLoaded = new Date(1989, 6, 25);
 
 test('reducer test - REQUEST_GAME', () => {
+  const initialState = Object.assign({}, defaultState);
   const action = {
     type: 'REQUEST_GAME',
   };
-  expect(huarteApp(defaultState, action)).toMatchSnapshot();
+  expect(huarteApp(initialState, action)).toMatchSnapshot();
 });
 
 test('reducer test - RECEIVE_GAME', () => {
+  const initialState = Object.assign({}, defaultState);
   const action = {
     type: 'RECEIVE_GAME',
     game,
@@ -23,41 +25,46 @@ test('reducer test - RECEIVE_GAME', () => {
     gameId: game.id,
   };
 
-  expect(huarteApp(defaultState, action)).toMatchSnapshot();
+  expect(huarteApp(initialState, action)).toMatchSnapshot();
 });
 
 test('reducer test - SELECT_GAME', () => {
+  const initialState = Object.assign({}, defaultState);
   const action = {
     type: 'SELECT_GAME',
     gameId: game.id,
   };
 
-  expect(huarteApp(defaultState, action)).toMatchSnapshot();
+  expect(huarteApp(initialState, action)).toMatchSnapshot();
 });
 
 test('reducer test - REQUEST_SEASONS', () => {
+  const initialState = Object.assign({}, defaultState);
   const action = {
     type: 'REQUEST_SEASONS',
   };
-  expect(huarteApp(defaultState, action)).toMatchSnapshot();
+  expect(huarteApp(initialState, action)).toMatchSnapshot();
 });
 
 test('reducer test - RECEIVE_SEASONS', () => {
+  const initialState = Object.assign({}, defaultState);
   const action = {
     type: 'RECEIVE_SEASONS',
     seasons: seasonList.seasons,
   };
-  expect(huarteApp(defaultState, action)).toMatchSnapshot();
+  expect(huarteApp(initialState, action)).toMatchSnapshot();
 });
 
 test('reducer test - REQUEST_GAME_LIST', () => {
+  const initialState = Object.assign({}, defaultState);
   const action = {
     type: 'REQUEST_GAME_LIST',
   };
-  expect(huarteApp(defaultState, action)).toMatchSnapshot();
+  expect(huarteApp(initialState, action)).toMatchSnapshot();
 });
 
 test('reducer test - RECEIVE_GAME_LIST', () => {
+  const initialState = Object.assign({}, defaultState);
   // it is assumed that we will first have done RECEIVE_SEASONS, so emulate that here
   const seasons = seasonList.seasons;
   seasons.forEach((season) => {
@@ -68,7 +75,7 @@ test('reducer test - RECEIVE_GAME_LIST', () => {
     type: 'RECEIVE_SEASONS',
     seasons: seasonList.seasons,
   };
-  const result = huarteApp(defaultState, receiveSeasonsAction);
+  const result = huarteApp(initialState, receiveSeasonsAction);
 
   const action = {
     type: 'RECEIVE_GAME_LIST',
@@ -81,18 +88,132 @@ test('reducer test - RECEIVE_GAME_LIST', () => {
 });
 
 test('reducer test - GLOBAL_SET_ERROR', () => {
+  const initialState = Object.assign({}, defaultState);
   const action = {
     type: 'GLOBAL_SET_ERROR',
     error: 'Something went wrong',
   };
 
-  expect(huarteApp(defaultState, action)).toMatchSnapshot();
+  expect(huarteApp(initialState, action)).toMatchSnapshot();
 });
 
 test('reducer test - GLOBAL_DISMISS_ERROR', () => {
+  const initialState = Object.assign({}, defaultState);
   const action = {
     type: 'GLOBAL_DISMISS_ERROR',
   };
-  expect(huarteApp(defaultState, action)).toMatchSnapshot();
+  expect(huarteApp(initialState, action)).toMatchSnapshot();
+});
+
+test('reducer test - SELECT_QUESTION', () => {
+  // assumes a game has been selected and received from the server as a prequisite
+  const initialState = Object.assign({}, defaultState);
+
+  const selectGameAction = {
+    type: 'SELECT_GAME',
+    gameId: game.id,
+  };
+  let state = huarteApp(initialState, selectGameAction);
+
+  const receiveGameAction = {
+    type: 'RECEIVE_GAME',
+    game,
+    timeLoaded,
+    gameId: game.id,
+  };
+  state = huarteApp(state, receiveGameAction);
+
+  const action = {
+    type: 'SELECT_QUESTION',
+    round: 'jeopardy',
+    categoryIndex: 0,
+    clueIndex: 0,
+  };
+  expect(huarteApp(state, action)).toMatchSnapshot();
+});
+
+test('reducer test - UPDATE_SCORE', () => {
+  // assumes a game has been selected and received from the server as a prequisite
+  const initialState = Object.assign({}, defaultState);
+
+  const selectGameAction = {
+    type: 'SELECT_GAME',
+    gameId: game.id,
+  };
+  let state = huarteApp(initialState, selectGameAction);
+
+  const receiveGameAction = {
+    type: 'RECEIVE_GAME',
+    game,
+    timeLoaded,
+    gameId: game.id,
+  };
+  state = huarteApp(state, receiveGameAction);
+
+  const action = {
+    type: 'UPDATE_SCORE',
+    delta: 200,
+  };
+
+  expect(huarteApp(state, action)).toMatchSnapshot();
+});
+
+test('reducer test - NEXT_ROUND', () => {
+  // assumes a game has been selected and received from the server as a prequisite
+  const initialState = Object.assign({}, defaultState);
+
+  const selectGameAction = {
+    type: 'SELECT_GAME',
+    gameId: game.id,
+  };
+  let state = huarteApp(initialState, selectGameAction);
+
+  const receiveGameAction = {
+    type: 'RECEIVE_GAME',
+    game,
+    timeLoaded,
+    gameId: game.id,
+  };
+  state = huarteApp(state, receiveGameAction);
+
+  const action = {
+    type: 'NEXT_ROUND',
+    nextRound: 'double_jeopardy',
+  };
+
+  expect(huarteApp(state, action)).toMatchSnapshot();
+});
+
+test('reducer test - BID_FINAL_JEOPARDY', () => {
+  // assumes a game has been selected and received from the server as a prequisite
+  const initialState = Object.assign({}, defaultState);
+
+  const selectGameAction = {
+    type: 'SELECT_GAME',
+    gameId: game.id,
+  };
+  let state = huarteApp(initialState, selectGameAction);
+
+  const receiveGameAction = {
+    type: 'RECEIVE_GAME',
+    game,
+    timeLoaded,
+    gameId: game.id,
+  };
+  state = huarteApp(state, receiveGameAction);
+
+  // might as well, to make it more realistic
+  const nextRoundAction = {
+    type: 'NEXT_ROUND',
+    nextRound: 'final_jeopardy',
+  };
+  state = huarteApp(state, nextRoundAction);
+
+  const action = {
+    type: 'BID_FINAL_JEOPARDY',
+    bid: 1500,
+  };
+
+  expect(huarteApp(state, action)).toMatchSnapshot();
 });
 
