f0220427f158ae530c2742d9057463b96448c986
Alex Pelan <alexpelan@gmail.com>
2017-09-23 19:06:32 -0400

    Lint

--- __tests__/Actions--test.js
+++ __tests__/Actions--test.js
@@ -1,10 +1,6 @@
 import 'react-native';
+import MockDate from 'mockdate';
 import Factory from '../testHelpers/Factory';
-import MockDate from "mockdate";
-
-const game = Factory.game();
-const gameList = Factory.gameList();
-const seasonList = Factory.seasonList();
 
 import {
   requestGame,
@@ -21,70 +17,72 @@ import {
   setError,
   dismissError,
   noop,
-  fetchGame,
-  fetchGameList,
-  fetchSeasons,
-} from "../app/actions/index";
+} from '../app/actions/index';
+
+const game = Factory.game();
+const gameList = Factory.gameList();
+const seasonList = Factory.seasonList();
+
 
 it('creates a REQUEST_GAME action', () => {
-	expect(requestGame("5401")).toMatchSnapshot();
+  expect(requestGame('5401')).toMatchSnapshot();
 });
 
 it('creates a RECEIVE_GAME action', () => {
-	MockDate.set(1434319925275);
-	expect(receiveGame(game, "5401")).toMatchSnapshot();
-	MockDate.reset();
+  MockDate.set(1434319925275);
+  expect(receiveGame(game, '5401')).toMatchSnapshot();
+  MockDate.reset();
 });
 
 it('creates a SELECT_GAME action', () => {
-	expect(selectGame(game)).toMatchSnapshot();
+  expect(selectGame(game)).toMatchSnapshot();
 });
 
 it('creates a REQUEST_SEASONS action ', () => {
-	expect(requestSeasons()).toMatchSnapshot();
+  expect(requestSeasons()).toMatchSnapshot();
 });
 
 it('creates a RECEIVE_SEASONS action', () => {
-	MockDate.set(1434319925275);
-	expect(receiveSeasons(seasonList)).toMatchSnapshot();
-	MockDate.reset();
+  MockDate.set(1434319925275);
+  expect(receiveSeasons(seasonList)).toMatchSnapshot();
+  MockDate.reset();
 });
 
 it('creates a REQUEST_GAME_LIST action', () => {
-	expect(requestGameList()).toMatchSnapshot();
+  expect(requestGameList()).toMatchSnapshot();
 });
 
 it('creates a RECEIVE_GAME_LIST action', () => {
-	MockDate.set(1434319925275);
-	expect(receiveGameList(gameList, "33")).toMatchSnapshot();
-	MockDate.reset();
+  MockDate.set(1434319925275);
+  expect(receiveGameList(gameList, '33')).toMatchSnapshot();
+  MockDate.reset();
 });
 
 it('creates a SELECT_QUESTION action', () => {
-	expect(selectQuestion("jeopardy", 0, 0)).toMatchSnapshot();
+  expect(selectQuestion('jeopardy', 0, 0)).toMatchSnapshot();
 });
 
 it('creates an UPDATE_SCORE action', () => {
-	expect(updateScore(-200)).toMatchSnapshot();
+  expect(updateScore(-200)).toMatchSnapshot();
 });
 
 it('creates a NEXT_ROUND action', () => {
-	expect(nextRound("jeopardy")).toMatchSnapshot();
-	expect(nextRound("double_jeopardy")).toMatchSnapshot();
+  expect(nextRound('jeopardy')).toMatchSnapshot();
+  expect(nextRound('double_jeopardy')).toMatchSnapshot();
 });
 
 it('creates a BID_FINAL_JEOPARDY action', () => {
-	expect(bidFinalJeopardy(2000)).toMatchSnapshot();
+  expect(bidFinalJeopardy(2000)).toMatchSnapshot();
 });
 
 it('creates a GLOBAL_SET_ERROR action', () => {
-	expect(setError("Network error")).toMatchSnapshot();
+  expect(setError('Network error')).toMatchSnapshot();
 });
 
 it('creates a GLOBAL_DISMISS_ERROR action', () => {
-	expect(dismissError()).toMatchSnapshot();
+  expect(dismissError()).toMatchSnapshot();
 });
 
 it('creates a NOOP action', () => {
-	expect(noop()).toMatchSnapshot();
+  expect(noop()).toMatchSnapshot();
 });
